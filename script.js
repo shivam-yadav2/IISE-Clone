e2d2e7879f0f877b18968cf732868be293568a38
shivam-yadav2 <shivsupertech@gmail.com>
2023-10-06 13:25:16 +0530

    hero added

---
+++ script.js
@@ -0,0 +1,9 @@
+let nBtn = document.getElementById('n-btn');
+
+
+nBtn.addEventListener('mouseover' , ()=>{
+    let drop = document.getElementById('Drop');
+    // alert('hello');
+    drop.classList.add = 'dropdown2';
+    drop.classList.remove = 'dropdown';
+})
